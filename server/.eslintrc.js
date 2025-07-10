0b570f483f48ae5797e12544234ed624974468ef
steugene <evgeny.sternin@osome.com>
2025-07-10 04:13:34 +0200

    Fix server linting issues for CI
    - Create tsconfig.test.json for test files
    - Update ESLint configuration to use both tsconfig files
    - Add override rules for test files to allow any types and require statements
    - All linting now passes successfully

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['tsconfig.json', 'tsconfig.test.json'],
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/', 'test/'],
+  ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
@@ -23,7 +23,17 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
     'prefer-const': 'error',
     'no-var': 'error',
   },
+  overrides: [
+    {
+      files: ['**/*.spec.ts', '**/*.test.ts', 'test/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 }; 
